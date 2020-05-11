bfb5e6cd0252b58a328250383310146fce1ce0fd
sravel <sebastien.ravel@cirad.fr>
2020-05-11 15:28:44 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from io import open
+import os.path as osp
+from setuptools import setup
+
+
+HERE = osp.abspath(osp.dirname(__file__))
+sys.path.insert(0, HERE)
+import pibooth_google_photo as plugin
+
+
+def main():
+    setup(
+        name=plugin.__name__,
+        version=plugin.__version__,
+        description=plugin.__doc__,
+        long_description=open(osp.join(HERE, 'README.rst'), encoding='utf-8').read(),
+        long_description_content_type='text/x-rst',
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Other Environment',
+            'Intended Audience :: Developers',
+            'Intended Audience :: End Users/Desktop',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.6',
+            'Natural Language :: English',
+            'Topic :: Multimedia :: Graphics :: Capture :: Digital Camera',
+        ],
+        author="Sébastien Ravel, Vincent Verdeil, Antoine Rousseaux",
+        url="https://github.com/pibooth/pibooth-google-photo",
+        download_url="https://github.com/pibooth/pibooth-google-photo/archive/{}.tar.gz".format(plugin.__version__),
+        license='MIT license',
+        platforms=['unix', 'linux'],
+        keywords=[
+            'Raspberry Pi',
+            'camera',
+            'photobooth'
+        ],
+        py_modules=['pibooth-google-photo'],
+        install_requires=[
+            'pibooth>=1.2.0',
+            'google-auth-oauthlib>=0.4.1'
+        ],
+        options={
+            'bdist_wheel':
+                {'universal': True}
+        },
+        zip_safe=False,  # Don't install the lib as an .egg zipfile
+        entry_points={'pibooth': ["pibooth_google_photo = pibooth_google_photo"]},
+    )
+
+
+if __name__ == '__main__':
+    main()
